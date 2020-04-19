cb4b61442cec9cdc9fd09c9460c1d559522c9ab4
grajetapiotr@gmail.com <grajetapiotr@gmail.com>
2020-04-19 16:36:02 +0200

    session

--- WebForm1.aspx.cs
+++ WebForm1.aspx.cs
@@ -11,11 +11,27 @@ namespace CalcApp
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if(Session["resultValue"] != null)
+            {
+                lblresult.Text = Session["resultValue"].ToString();
+            }
+            if (Session["opt"] != null)
+            {
+                DropDownList1.SelectedValue = Session["opt"].ToString();
+            }
+            if (Session["num1"] != null)
+            {
+                txtNumber1.Text = Session["num1"].ToString();
+            }
+            if (Session["num2"] != null)
+            {
+                txtNumber2.Text = Session["num2"].ToString();
+            }
         }
 
         protected void btnSubmit_Click(object sender, EventArgs e)
         {
+
             var num1 = Convert.ToDecimal(this.txtNumber1.Text);
             var num2 = Convert.ToDecimal(this.txtNumber2.Text);
             var opt = this.DropDownList1.SelectedValue;
@@ -39,6 +55,11 @@ namespace CalcApp
                 result = num1 / num2;
             }
 
+            var resultValue = result.ToString();
+            Session["resultValue"] = resultValue;
+            Session["opt"] = opt;
+            Session["num1"] = num1;
+            Session["num2"] = num2;
             lblresult.Text = result.ToString();
         }
     }
